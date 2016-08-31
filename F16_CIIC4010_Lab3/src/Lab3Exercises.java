81a8022730bf6f8d2fc56152120b8ebcfbf6c06a
4015c9 <4015c9@coco.ece.uprm.edu>
2016-08-31 17:48:30 -0400

    Changed oval to drawn polygon(star in this case)

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -3,9 +3,16 @@ import javax.swing.JFrame;
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Melvin Malave");
+	
+		
+		
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(900, 450);
 		myFrame.setSize(200, 200);
+		 Mypanelclass myPanel = new Mypanelclass();
+         myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
+	
+	
 	}
 }
